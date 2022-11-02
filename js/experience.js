fe548eed02591435981a20f5c6d64dce09242541
sosophia10 <68720697+sosophia10@users.noreply.github.com>
2022-11-02 10:39:32 -0400

    files added

---
+++ js/experience.js
@@ -0,0 +1,34 @@
+    $(document).ready(function () {
+        var intCounter = 1;
+        var className = $('#openbuttonapp').attr('class');
+
+
+        $('#openbuttonapp').click(function () {
+            var $objWindow = $('<div class="window">Window ' + intCounter + '<br>&nbsp;Welcome to my Time Capsule!<b>(2018-2022)</b><br><br>  <b>2018</b><br>&nbsp;  -Purple Lightsaber (After Effects)   <br><br><br></div>');
+
+            var intRandom = Math.floor((Math.random() * 12) + 1);
+            $($objWindow).appendTo('body');
+            $objWindow.window({
+                title: className,
+                width: 480,
+                height: 320,
+                position: {
+                    my: 'left+' + 200 + ', top+' + 200,
+                    at: 'left top',
+                    of: window
+                },
+                maximizable: true,
+                minimizable: true,
+
+                icon: '../src/jquery-lwd/themes/windows2000/images/icons/6.png'
+            });
+
+            intCounter++;
+        });
+
+        $('#taskbar').taskbar();
+
+        $('#theme').change(function () {
+            $("head link#themecss").attr("href", $(this).val());
+        });
+    });
