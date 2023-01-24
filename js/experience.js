3475cf7c77ea178743ea7d79fbd40df8177eafac
Sophia <68720697+sosophia10@users.noreply.github.com>
2023-01-24 02:09:11 -0500

    Update experience.js

--- js/experience.js
+++ js/experience.js
@@ -26,32 +26,4 @@
             intCounter++;
         });
 
-        $('#experience').dblclick(function () {
-
-            var $objWindow = $('<iframe style="width:98.8%; min-width:98.8%;" src="apps/experience.html"></iframe>');
-
-            $($objWindow).appendTo('body');
-            $objWindow.window({
-                title: className,
-                width: 800,
-                height: 600,
-                position: {
-                    my: 'left+' + 200 + ', top+' + 200,
-                    at: 'left top',
-                    of: window
-                },
-                maximizable: true,
-                minimizable: true,
-
-                icon: '../src/jquery-lwd/themes/sophia/images/icons/13.png'
-            });
-
-            intCounter++;
-        });
-
-        $('#taskbar').taskbar();
-
-        $('#theme').change(function () {
-            $("head link#themecss").attr("href", $(this).val());
-        });
     });
